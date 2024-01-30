2b7339e4826295149d1873141e8c46849ca62411
youssefsayyad <156002599+youssefsayyad@users.noreply.github.com>
2024-01-30 19:22:59 +0200

    Add files via upload

---
+++ AUTOSAR_Project/Debug/Port.d
@@ -0,0 +1,30 @@
+# FIXED
+
+Port.obj: ../Port.c
+Port.obj: ../Port.h
+Port.obj: ../Std_Types.h
+Port.obj: ../Platform_Types.h
+Port.obj: ../Compiler.h
+Port.obj: ../Port_Cfg.h
+Port.obj: ../Common_Macros.h
+Port.obj: ../Port_Regs.h
+Port.obj: ../Det.h
+
+../Port.c:
+
+../Port.h:
+
+../Std_Types.h:
+
+../Platform_Types.h:
+
+../Compiler.h:
+
+../Port_Cfg.h:
+
+../Common_Macros.h:
+
+../Port_Regs.h:
+
+../Det.h:
+
